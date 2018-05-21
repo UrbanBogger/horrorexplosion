7d4a5dc9be2dc19dd5b173f30ab734dcf9194169
emitjsu <mitja.suncic@ericsson.com>
2018-05-21 16:52:43 +0100

    updated the settings file to reflect the new domain name

--- horrorexplosion/settings.py
+++ horrorexplosion/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['horrorexplosion.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['horrorexplosion.com', '127.0.0.1']
 
 # Application definition
 
