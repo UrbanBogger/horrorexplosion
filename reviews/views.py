9ae41d0bb0216aef2ddeb5986cef0fb6c2562a94
emitjsu <mitja.suncic@ericsson.com>
2018-06-23 20:00:13 +0100

    Have modified the models so that they accommodate the addition of the 'Article' model

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views import generic
-from django.core.paginator import Paginator
 from .models import Movie, MovieReview, WebsiteMetadescriptor,ReferencedMovie, \
     Contributor, get_random_review
 
