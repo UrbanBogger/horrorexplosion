3f349e08a492043d8d93bf2b8712707287716d49
emitjsu <mitja.suncic@ericsson.com>
2018-12-02 12:37:04 +0000

    Adjusting similar movies list sort 1

--- reviews/views.py
+++ reviews/views.py
@@ -383,7 +383,8 @@ def calculate_bonus_similarity_pts(similar_mov_list, movie):
              'bonus_similarity_points': bonus_similarity_points})
     # adjust similar movies based on bonus points
     return sorted(mov_similarity_list, key=itemgetter(
-        'similarity_percentages', 'bonus_similarity_points'), reverse=True)
+        'bonus_similarity_points',
+        'similarity_percentages'), reverse=True)
 
 
 def get_similar_movies(movie):
