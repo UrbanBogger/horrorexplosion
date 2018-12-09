656500f4ad0984c5ad9279de573d2ec62083aab3
emitjsu <mitja.suncic@ericsson.com>
2018-12-09 10:50:42 +0000

    Have introduced the 'find_similar_movies' command to the 'reviews' app

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py find_similar_movies
 web: gunicorn horrorexplosion.wsgi --log-file -
--- reviews/apps.py
+++ reviews/apps.py
@@ -5,7 +5,5 @@ class ReviewsConfig(AppConfig):
     name = 'reviews'
 
     def ready(self):
-        from mov_similarity_calculator import calculate_movie_similarity
-        calculate_movie_similarity.find_and_save_similar_movies()
         from mov_similarity_calculator import similar_mov_updater
         similar_mov_updater.start()
