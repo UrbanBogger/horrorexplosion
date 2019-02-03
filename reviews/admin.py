1d1998512b139394bd4049bd0b37be48e356bc44
emitjsu <mitja.suncic@ericsson.com>
2019-02-03 15:13:52 +0000

    Have registered the new TV-related DB object with the Admin page

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,7 +3,8 @@ from .models import Genre, Subgenre, Microgenre, Keyword, \
 WebsiteMetadescriptor, Reviewer, MovieCreator, Country, \
     CreativeRole, Movie, MovieParticipation, Title, MovieFranchise, \
     MovieReview, ReferencedMovie, Grade, Contributor, Article, MovieRemake, \
-    MovieInMovSeries, MovieSeries, SimilarMovie, PickedReview
+    MovieInMovSeries, MovieSeries, SimilarMovie, PickedReview, \
+    TelevisionSeries, TelevisionSeason
 
 # Register your models here.
 admin.site.register(Genre)
@@ -27,6 +28,7 @@ admin.site.register(Article)
 admin.site.register(MovieRemake)
 admin.site.register(MovieInMovSeries)
 admin.site.register(MovieSeries)
-#admin.site.register(TelevisionSeries)
+admin.site.register(TelevisionSeries)
+admin.site.register(TelevisionSeason)
 admin.site.register(SimilarMovie)
 admin.site.register(PickedReview)
