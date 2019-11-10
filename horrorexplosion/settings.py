8bd8c4e1ee8c39ba07b2cb2bcc67bc5e92af5708
UrbanBogger <mitja.suncic@gmail.com>
2019-11-10 13:51:10 +0000

    Have set Django DEBUG variable to 'False' by default 1

--- horrorexplosion/settings.py
+++ horrorexplosion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['www.horrorexplosion.com', 'horrorexplosion.herokuapp.com',
                  '127.0.0.1']
